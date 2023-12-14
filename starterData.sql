b1d4f1ba3f337a592caa2f64ccd25e1914bc185f
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2023-12-13 21:12:01 -0500

    Created initial Dockerfile and sql data

---
+++ starterData.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE Students;
+USE Students;
+
+CREATE TABLE student (
+  StudentID INT NOT NULL AUTO_INCREMENT,
+  FirstName VARCHAR(100) NOT NULL,
+  LastName VARCHAR(100) NOT NULL,
+  PRIMARY KEY (StudentID)
+);
+
+INSERT INTO student (FirstName, LastName) VALUES
+  ("John", "Smith"),
+  ("Emma", "Stone");
